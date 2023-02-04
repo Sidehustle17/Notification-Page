04c10e2137848716a8d6e86fd3c6e2028f8e4b2f
Dev_King <100457552+Sidehustle17@users.noreply.github.com>
2023-02-04 20:19:47 +0100

    Add files via upload

---
+++ Notification Page with JS/code/index.js
@@ -0,0 +1,13 @@
+
+
+function read(){
+   document.getElementById("text").style.backgroundColor = "#ffff";
+   document.getElementById("ang").style.backgroundColor = "#ffff";
+   document.getElementById("thom").style.backgroundColor = "#ffff";
+
+   document.getElementById("dot").style.display = "none";
+   document.getElementById("dote").style.display = "none";
+   document.getElementById("dotes").style.display = "none";
+
+   document.getElementById("alert").textContent = 0;
+}
